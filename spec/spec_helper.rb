9df0bffe8b1f858979c26ad655a9e166eecd0abd
Jeff Luckett <jluckett@dataxu.com>
2017-10-27 15:40:43 -0400

    Require tempfile and socket to make specs run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'delayed_job_worker_pool'
+require 'tempfile'
+require 'socket'
 
 Dir['spec/support/**/*.rb'].each { |f| require File.expand_path(f) }
